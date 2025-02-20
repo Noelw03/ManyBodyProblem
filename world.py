2396a8c7b09bc80ccbe3e54dc7d8055f3b62f058
Noel Wachtel <155498324+Noelw03@users.noreply.github.com>
2025-02-20 17:49:25 +0000

    add record_simulation_step method to World class for tracking simulation state

--- world.py
+++ world.py
@@ -14,4 +14,15 @@ class World:
     def border(self, size):
         """Creates a border if the user decides to implement a position limit"""
 
+    def record_simulation_step(self, time_step):
+        """Record the current state of the simulation.
+        """
+        for i, body in enumerate(self.humans):
+                self.simuation_data["time_step"].append(time_step)
+                self.simuation_data["body_index"].append(i)
+                self.simuation_data["position_x"].append(body.position[0])
+                self.simuation_data["position_y"].append(body.position[1])
+                self.simuation_data["position_z"].append(body.position[2])
+
+
         
