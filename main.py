cdb8b2c6e09e9385e56bec38aa9f633e544cf0a6
Noel Wachtel <155498324+Noelw03@users.noreply.github.com>
2025-04-28 12:56:26 +0000

    implemented option for the user to specify more values in the simulation

--- main.py
+++ main.py
@@ -7,21 +7,44 @@ def main():
     """Main function that initializes the bodies and calculates the simulation data"""
 
     border_yes_or_no = input("Would you like to implement a border? (yes/no): ")
-    size_ = float(input("Enter the size of the world: "))
-    dt_ = float(input("Enter the time step: "))
+    size_ = float(input("Enter the size of the world (press 'Enter' for the standard size of 10^6): "))
+    if size_ == "":
+        size_ = 1e6
+    else:
+        size_ = float(size_)
+    dt_ = float(input("Enter the time step (press 'Enter' for the recommended timestep of 0.01): "))
+    if dt_ == "":
+        dt_ = 0.01
+    else:
+        dt_ = float(dt_)
     time_steps = int(input("Enter the number of time steps: "))
-    body_count = int(input("Enter the number of bodies: "))
+    body_count = int(input("Enter the number of bodies (just press 'Enter' for the 3-body-problem): "))
 
     # Initialize the world
     world = World(size = size_, dt = dt_)
 
     random_or_not = input("Would you like to use predefined intervals (type 1) for the values or define them yourself (type 2): ")
-    if random_or_not == "1":
+    if random_or_not == "1": # Predefined intervals
         for i in range(body_count):
             mass1 = 1e25
             position1 = np.array([np.random.uniform(-world.size/1.5, world.size/1.5), np.random.uniform(-world.size/1.5, world.size/1.5), np.random.uniform(-world.size/1.5, world.size/1.5)])
             velocity1 = np.array(np.array([np.random.uniform(-2, 2), np.random.uniform(-2, 2), np.random.uniform(-2, 2)]))
             acceleration1 = np.array([np.random.uniform(-2, 2), np.random.uniform(-2, 2), np.random.uniform(-2, 2)])
+            world.bodies.append(Body(mass=mass1, position=position1, velocity=velocity1, acceleration=acceleration1))
+
+    if random_or_not == "2": # User-defined intervals
+        min_mass_range = float(input("Enter the min. mass of the bodies "))
+        max_mass_range = float(input("Enter the max. mass of the bodies "))
+        min_velocity_range = float(input("Enter the min. velocity of the bodies "))
+        max_velocity_range = float(input("Enter the max. velocity of the bodies "))
+        min_acceleration_range = float(input("Enter the min. acceleration of the bodies "))
+        max_acceleration_range = float(input("Enter the max. acceleration of the bodies "))
+
+        for i in range(body_count):
+            mass1 = np.array([np.random.uniform(min_mass_range, max_mass_range), np.random.uniform(min_mass_range, max_mass_range), np.random.uniform(min_mass_range, max_mass_range)])
+            position1 = np.array([np.random.uniform(-world.size/1.5, world.size/1.5), np.random.uniform(-world.size/1.5, world.size/1.5), np.random.uniform(-world.size/1.5, world.size/1.5)])
+            velocity1 = np.array([np.random.uniform(min_velocity_range, max_velocity_range), np.random.uniform(min_velocity_range, max_velocity_range), np.random.uniform(min_velocity_range, max_velocity_range)])
+            acceleration1 = np.array([np.random.uniform(min_acceleration_range, max_acceleration_range), np.random.uniform(-min_acceleration_range, max_acceleration_range), np.random.uniform(min_acceleration_range, max_acceleration_range)])
             world.bodies.append(Body(mass=mass1, position=position1, velocity=velocity1, acceleration=acceleration1))
 
     for n in range(time_steps):  # Updates the simulation for every time step
